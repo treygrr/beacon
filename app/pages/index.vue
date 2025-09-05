102723ddaf5967f1d307bb9ea6ab810cdbd827d7
Treygrr <phlasc@gmail.com>
2025-09-05 15:53:39 -0400

    jidk

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 const runtimeConfig = useRuntimeConfig()
 const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
 const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
@@ -9,6 +10,7 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
     <h1 :style="{ color }">
       {{ runtimeConfig.public.helloText }}
     </h1>
+    {{ useNuxtApp().$config.public.betterAuthUrl }}
     <NuxtLink
       to="/"
       external
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   // https://nuxt.com/modules
   modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui'],
 
+  nitro: {
+    experimental: {
+      tasks: true
+    }
+  },
   // https://devtools.nuxt.com
   devtools: { enabled: true },
 
@@ -10,7 +15,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋'
+      helloText: 'Hello from the Edge 👋',
+      betterAuthUrl: process.env.BETTER_AUTH_URL,
     }
   },
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
@@ -18,7 +24,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-03-01',
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {},
+  hub: {
+    database: true
+  },
 
   // Development config
   eslint: {
